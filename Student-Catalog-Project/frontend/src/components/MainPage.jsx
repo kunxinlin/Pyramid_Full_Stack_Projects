5660493198e0cfeadd1a033ef4058748ce38a802
Kris Lin <kunxinlin@gmail.com>
2020-08-26 10:37:32 -0400

    added table showing students on mainpage

--- Student-Catalog-Project/frontend/src/components/MainPage.jsx
+++ Student-Catalog-Project/frontend/src/components/MainPage.jsx
@@ -1,34 +1,58 @@
-import React, { Component } from 'react';
-import {withRouter} from 'react-router-dom';
-import StudentDataService from '../service/StudentDataService';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import StudentDataService from "../service/StudentDataService";
+import { Card } from "react-bootstrap";
 
 class MainPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            students: [],
-            message: null,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [],
+      message: null,
+    };
+  }
 
-    componentDidMount(){
-        this.refreshStudentList();
-    }
+  componentDidMount() {
+    this.refreshStudentList();
+  }
 
-    refreshStudentList(){
-        StudentDataService.retrieveAllStudents().then(response => {this.setState({students: response.data})})
-    }
+  refreshStudentList() {
+    StudentDataService.retrieveAllStudents().then((response) => {
+      this.setState({ students: response.data });
+    });
+  }
 
-    render() {
-        return(
-            <div className="container">
-             <h1>STUDENT CATALOG</h1>
-             <br/><br/>
-            </div>
-        )
+  render() {
+    return (
+      <div className="container">
+        <br />
+        <br />
+        <h1>STUDENT CATALOG</h1>
+        <br />
+        <br />
 
-    }
+        <table class="table table-striped">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                this.state.students.map(student =>
+                    <tr>
+                        <td>{student.id}</td>
+                        <td>{student.firstName}</td>
+                        <td>{student.lastName}</td>
+                    </tr>)
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
 
 export default withRouter(MainPage);
